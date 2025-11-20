4662d1e9f5fa168dba643a4dcd91fcab3009ed4e
fersanz_06 <luisfernandosanchezbriseno052@gmail.com>
2025-11-19 21:40:20 -0600

    Commit 1

---
+++ HashTable.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+
+public class HashTable {
+    public static void main(String[] args) {
+        HashMap<Integer, String> mapa = new HashMap<>();
+        mapa.put(1, "Luis");
+        mapa.put(2, "Andrea");
+        mapa.put(3, "Fernando");
+        mapa.put(4, "Carlos");
+
+        System.out.println("Tabla original: " + mapa);
+    }
+}
